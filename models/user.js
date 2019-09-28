2211173c1b09886eb0afbb2d18b261ff6c1b3cbc
Lu <lu@hooli.store>
2019-09-28 03:15:07 +0200

    Fixed error

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
-
+console.log('tuh');
 const userSchema = new Schema({
   name: {
     type: String,
