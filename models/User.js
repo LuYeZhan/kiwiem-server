2f98c27f729f9becd74c371d40ef4c1a54b8c971
Lu <lu@hooli.store>
2019-09-28 03:18:32 +0200

    fix Users

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  surname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  userType: {
+    type: String,
+    enum: ['volunteer', 'traveller']
+  },
+  pocket: {
+    type: Number
+  },
+  comments: [{
+    type: ObjectId,
+    ref: 'Comment'
+  }],
+  myTrips: [{
+    type: ObjectId,
+    ref: 'Trip'
+  }]
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
