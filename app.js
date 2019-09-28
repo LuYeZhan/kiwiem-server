c66f57af6afab5861307c72c2f33361a930bc96b
Lu <lu@hooli.store>
2019-09-28 15:16:04 +0200

    cors

--- app.js
+++ app.js
@@ -28,17 +28,17 @@ mongoose
 
 const app = express();
 
-app.use(
-  cors()
-);
+// app.use(
+//   cors()
+// );
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', process.env.PUBLIC_DOMAIN);
-//   res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS,DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', process.env.PUBLIC_DOMAIN);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.use(
   session({
