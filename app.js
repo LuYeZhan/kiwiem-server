454ccc40bdf905060004967225abe0362e5da79c
Lu <lu@hooli.store>
2019-09-28 15:22:14 +0200

    cors

--- app.js
+++ app.js
@@ -32,13 +32,13 @@ const app = express();
 //   cors()
 // );
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', process.env.PUBLIC_DOMAIN);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', process.env.PUBLIC_DOMAIN);
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS,DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
 
 app.use(
   session({
