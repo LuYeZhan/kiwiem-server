1a5aece1eab79d9c4025f0034f427f8cf0be2949
Lu <lu@hooli.store>
2019-09-29 10:59:31 +0200

    cors fix

--- app.js
+++ app.js
@@ -28,9 +28,12 @@ mongoose
 
 const app = express();
 
-// app.use(
-//   cors()
-// );
+app.use(
+  cors({
+    credentials: true,
+    origin: true
+  })
+);
 
 // app.use((req, res, next) => {
 //   res.setHeader('Access-Control-Allow-Origin', process.env.PUBLIC_DOMAIN);
